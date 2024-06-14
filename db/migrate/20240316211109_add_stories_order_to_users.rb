7868141ad55133450bdb8aa9b01c58b9e2badfc4
ASUN <37107613+HelloAngela@users.noreply.github.com>
2024-06-14 12:10:55 +1000

    Update 20240316211109_add_stories_order_to_users.rb
    change column type to enum to avoid migration issues

--- db/migrate/20240316211109_add_stories_order_to_users.rb
+++ db/migrate/20240316211109_add_stories_order_to_users.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+# class AddStoriesOrderToUsers < ActiveRecord::Migration[7.1]
+#   def change
+#     add_column :users, :stories_order, :string, default: "desc"
+#   end
+# end
+
+
 class AddStoriesOrderToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :stories_order, :string, default: "desc"
+    add_column :users, :stories_order, :integer, default: 0 
   end
 end
